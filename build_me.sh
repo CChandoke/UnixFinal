6acca2de6c4d56964c98daefe510435287f89dad
Cameron Chandoke <cac094120@utdallas.edu>
2018-12-15 13:33:39 -0600

    dealer -o option produces seg fault

---
+++ build_me.sh
@@ -0,0 +1,6 @@
+# build dealer
+g++ -w -std=c++11 -o dealer dealer.cpp -I$HOME/local/gsl-2.5/include/
+
+# build hand
+gcc -w -o hand hand.c -L$HOME/local/gsl-2.5/lib/ -lgsl -lgslcblas  -I$HOME/local/gsl-2.5/include/ -Wl,-rpath=$HOME/local/gsl-2.5/lib/
+
