d9b2c0027787b1868464921fe40e35b1fe5c6a3f
CChandoke <44755739+CChandoke@users.noreply.github.com>
2018-12-17 06:24:29 -0600

    factorialApprox returns correct errorRatios!!

--- factorialApprox.cpp
+++ factorialApprox.cpp
@@ -14,11 +14,11 @@ int main()
 	printf("N\t\tln(Factorial)\t\tStirling's Formula\tError\t\t\tError Percent\n");
 	for (double i = 1; i <= x; i++)
 		{	
-		lnfact = gsl_sf_lngamma(i+1);  		// calculate ln(N!)
-		approx = i * log(i) - i;			// approximate ln(N!) using Stirling's formula
-		error = lnfact - approx;  			// calculate approximation error 
-		errorRatio = error / lnfact * 100;	// calculate percent approximation error
-		printf("%d\t\t%.6e\t\t%.6e\t\t%.6e\t\t%.d\n", static_cast<int>(i), lnfact, approx, error, errorRatio);
+		lnfact = gsl_sf_lngamma(i+1);  			// calculate ln(N!)
+		approx = i * log(i) - i;				// approximate ln(N!) using Stirling's formula
+		error = fabs(approx - lnfact);  		// calculate approximation error 
+		errorRatio = error / fabs(approx) * 100;		// calculate percent approximation error
+		printf("%d\t\t%.6e\t\t%.6e\t\t%.6e\t\t%.6e\n", static_cast<int>(i), lnfact, approx, error, errorRatio);
 		}
 	}
 
